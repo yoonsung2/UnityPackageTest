441d47cdd2fd0ff870df2f17bee418b50f13bae9
yoonsung <yoonsung@kyowon.co.kr>
2023-10-23 15:34:09 +0900

    package.json, PackageManager.cs 파일 추가

---
+++ PackageManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageManager : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("I'm Package Manager");
+    }
+
+}
